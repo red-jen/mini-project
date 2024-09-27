22c3e4cdc1ac634f4e37276e77874829047d2d1b
red-jen <redaje102105@gmail.com>
2024-09-27 19:17:04 +0100

    adddmore

---
+++ library.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+
+
+struct book {
+    char title[100];
+    char author[50];
+    float price;
+    int quantity;
+};
+struct book library[100];
+int bookCount = 0;
+
+int main(){
+    int 
+
+ printf("entrez le titre de livre");
+
+
+
+}
